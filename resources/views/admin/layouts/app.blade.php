8e6ed3a9526a2d2ea3b0574b757359453c05bf5e
yuyuan1213 <david1213117@gmail.com>
2019-12-01 00:46:16 +0800

    updated

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel='icon' href='{{asset('img/lottory.ico')}}' type='image/x-icon'/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel='icon' href='{{asset('img/lottory.ico')}}' type='image/x-icon'/>
 
     <title>Register | {{ config('app.name', 'Lottery') }}</title>
 
